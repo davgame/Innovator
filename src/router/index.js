2d80ced9565c7e7d6b0ad10273383e97e95283e4
davgame <davshorvoglyan@gmail.com>
2026-02-06 20:39:45 +0300

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,9 @@ import Home from '@/components/Home.vue'
 import FAQ from '@/components/FAQ/FAQ.vue' // путь зависит от структуры проекта
 import Contact from '@/components/Contact/Contact.vue'
 import SliderR from '@/components/Authorization/SliderR.vue'
-import Start from '@/components/SUP/Start.vue'
-import CreateTeam from '@/components/SUP/Create-Team.vue'
-import Pasport from '@/components/SUP/Pasport.vue'
+import Start from '@/components/Pasport/Start.vue'
+import CreateTeam from '@/components/Pasport/Create-Team.vue'
+import Pasport from '@/components/Pasport/Pasport.vue'
 
 
 const router = createRouter({
