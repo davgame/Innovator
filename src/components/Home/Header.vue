57e733390bfc6073f7065c95ddb51da8d5130c8d
davgame <davshorvoglyan@gmail.com>
2026-04-07 13:02:59 +0300

    Update

--- src/components/Home/Header.vue
+++ src/components/Home/Header.vue
@@ -231,7 +231,7 @@
             </a>
             <a href="#" target="_blank">
               <img
-                src="/src/assets/images/whatsApp.png"
+                src="@/assets/images/whatsapp.png"
                 alt="whatsApp"
                 class="w-10 h-10 block lg:hidden"
               />
